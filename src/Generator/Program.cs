341cf8984e2e7681c934362eb244e267374366f7
Dennis Dietrich <ddietrich@microsoft.com>
2023-01-23 12:09:59 -0800

    Added functional implementation of generator

--- src/Generator/Program.cs
+++ src/Generator/Program.cs
@@ -2,28 +2,35 @@
 #define FP
 
 using Generator;
-using Generator.Oop;
 
 const int iterations = 5;
 const int interval = 3;
 var startTime = new DateTime(2023, 01, 23, 20, 28, 0);
 
 #if OOP
-Console.WriteLine($"Generating {iterations} times with {interval} seconds interval using object-oriented implementation...");
-
-using var times = TimeGenerator.Create(startTime, interval).GetEnumerator();
-for (var i = 0; i < iterations; i++)
 {
-    times.MoveNext();
-    Console.WriteLine($"{times.Current:u}");
-    //Console.WriteLine($"{times.GetNext():u}");
-}
+    Console.WriteLine($"Generating {iterations} times with {interval} seconds interval using object-oriented implementation...");
 
-Console.WriteLine();
+    using var times = TimeGenerator.CreateEnumerable(startTime, interval).GetEnumerator();
+    for (var i = 0; i < iterations; i++)
+    {
+        times.MoveNext();
+        Console.WriteLine($"{times.Current:u}");
+        //Console.WriteLine($"{times.GetNext():u}");
+    }
+
+    Console.WriteLine();
+}
 #endif
 
 #if FP
-Console.WriteLine($"Generating {iterations} times with {interval} seconds interval using functional implementation...");
+{
+    Console.WriteLine($"Generating {iterations} times with {interval} seconds interval using functional implementation...");
+
+    var times = TimeGenerator.CreateFunction(startTime, interval);
+    for (var i = 0; i < iterations; i++)
+        Console.WriteLine($"{times():u}");
+}
 #endif
 
 #if !(OOP || FP)
---
+++ src/Generator/TimeGenerator.cs
@@ -0,0 +1,31 @@
+﻿namespace Generator
+{
+    public static class TimeGenerator
+    {
+        public static IEnumerable<DateTime> CreateEnumerable(DateTime start, int interval)
+        {
+            if (interval < 1)
+                throw new ArgumentOutOfRangeException(nameof(interval), "Interval must be greater than zero.");
+
+            while (true)
+            {
+                var nextTime = start.AddSeconds(interval);
+                yield return start;
+                start = nextTime;
+            }
+        }
+
+        public static Func<DateTime> CreateFunction(DateTime start, int interval)
+        {
+            if (interval < 1)
+                throw new ArgumentOutOfRangeException(nameof(interval), "Interval must be greater than zero.");
+
+            return () =>
+            {
+                var currentTime = start;
+                start = start.AddSeconds(interval);
+                return currentTime;
+            };
+        }
+    }
+}
