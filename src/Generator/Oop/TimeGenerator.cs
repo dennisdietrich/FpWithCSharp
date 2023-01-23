2da86868d8f18139798c1d7005b12ba136f5fd93
Dennis Dietrich <ddietrich@microsoft.com>
2023-01-23 11:59:27 -0800

    Initial check in of generator example

---
+++ src/Generator/Oop/TimeGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace Generator.Oop
+{
+    public static class TimeGenerator
+    {
+        public static IEnumerable<DateTime> Create(DateTime start, int interval)
+        {
+            if (interval < 1)
+                throw new ArgumentOutOfRangeException(nameof(interval), "Interval must be greater than zero.");
+
+            while (true)
+            {
+                var nextTime = start.AddSeconds(interval);
+                yield return start;
+                start = nextTime;
+            }
+        }
+    }
+}
---
+++ src/Generator/Program.cs
@@ -0,0 +1,31 @@
+﻿#define OOP
+#define FP
+
+using Generator;
+using Generator.Oop;
+
+const int iterations = 5;
+const int interval = 3;
+var startTime = new DateTime(2023, 01, 23, 20, 28, 0);
+
+#if OOP
+Console.WriteLine($"Generating {iterations} times with {interval} seconds interval using object-oriented implementation...");
+
+using var times = TimeGenerator.Create(startTime, interval).GetEnumerator();
+for (var i = 0; i < iterations; i++)
+{
+    times.MoveNext();
+    Console.WriteLine($"{times.Current:u}");
+    //Console.WriteLine($"{times.GetNext():u}");
+}
+
+Console.WriteLine();
+#endif
+
+#if FP
+Console.WriteLine($"Generating {iterations} times with {interval} seconds interval using functional implementation...");
+#endif
+
+#if !(OOP || FP)
+Console.WriteLine("Nothing to do...");
+#endif
