497620601e0de43b58361ff6a16b4667e41269c5
Dennis Dietrich <ddietrich@microsoft.com>
2023-02-17 17:43:39 -0800

    Initial check in of demo FunWithTracing

---
+++ src/FunWithTracing/LowLevelApiNoTracing.cs
@@ -0,0 +1,28 @@
+﻿namespace FunWithTracing
+{
+    public sealed class LowLevelApiNoTracing
+    {
+        public TimeSpan DefaultTtl { get; set; }
+
+        public IList<FileInfo> GetExpiredFiles()
+        {
+            List<FileInfo> expiredFiles = new();
+
+            foreach (var file in GetAllFiles())
+            {
+                var effectiveTtl = EffectiveTtl(file);
+                var lastAccess = File.GetLastAccessTimeUtc(file.FullName);
+                var isExpired = lastAccess + effectiveTtl < DateTime.UtcNow;
+                // What if we want to trace here?
+                if (isExpired)
+                    expiredFiles.Add(file);
+            }
+
+            return expiredFiles;
+        }
+
+        // Dummies so we have something to call representing further business logic
+        private IEnumerable<FileInfo> GetAllFiles() => Enumerable.Empty<FileInfo>();
+        private TimeSpan EffectiveTtl(FileInfo file) => DefaultTtl;
+    }
+}
