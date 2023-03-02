9f0b527e3ad2a50b8b23f227dd2f39401c3a4fb3
Dennis Dietrich <ddietrich@microsoft.com>
2023-03-01 22:41:31 -0800

    Initial check in of MethodCoreInjection project

---
+++ src/MethodCoreInjection/Program.cs
@@ -0,0 +1,34 @@
+﻿// Term "Method-Core Injection" coined by Ann Lewkowicz
+// http://www.annlewkowicz.com/2022/12/method-core-injection-c-pattern-for.html
+
+using System.Text.Json;
+
+namespace MethodCoreInjection
+{
+    public record Session(string Speaker, string Title);
+
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            var session = new Session("Dennis Dietrich", "So you think you know functions");
+            WriteToJsonFile("session.json", session);
+            WriteToTxtFile("session.txt", session);
+        }
+
+        private static void WriteToJsonFile(string filename, Session session)
+        {
+            using var fileStream = new FileStream(filename, FileMode.CreateNew);
+            JsonSerializer.Serialize(fileStream, session);
+            File.SetAttributes(filename, FileAttributes.ReadOnly);
+        }
+
+        private static void WriteToTxtFile(string filename, Session session)
+        {
+            using var fileStream = new FileStream(filename, FileMode.CreateNew);
+            using var streamWriter = new StreamWriter(fileStream);
+            streamWriter.Write($"{session.Speaker}: {session.Title}");
+            File.SetAttributes(filename, FileAttributes.ReadOnly);
+        }
+    }
+}
